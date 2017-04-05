364b55981d4911e8cb3e490e08816bf9ec69af46
Joe Howard <succinction@gmail.com>
2017-04-04 18:40:04 -0700

    bool game

---
+++ bool_game.py
@@ -0,0 +1,29 @@
+one = [2,1,2,3,4]
+
+# print()
+
+
+# List comprehension
+# two = [x * 10 for x in one]
+
+list_comp = [n % 2 for n in one]
+
+print(list_comp, sum(list_comp))
+# print(two)
+
+
+
+
+
+
+
+
+
+print( sum([1 - n % 2 for n in nums]))
+
+
+
+
+
+
+
